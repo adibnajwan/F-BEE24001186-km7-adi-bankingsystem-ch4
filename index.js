7850b4dc204429c40ea2f58ccf0f560528cae937
adibnajwan <adibnajwan@gmail.com>
2024-10-18 21:13:18 +0700

    Makeover index.js with add endpoint, prisma, and start user

--- index.js
+++ index.js
@@ -1,13 +1,33 @@
 const express = require('express');
+const { PrismaClient } = require('@prisma/client'); 
 const app = express();
-const port = 3000;
+const prisma = new PrismaClient(); 
 
-app.use(express.json()); // Middleware untuk parsing JSON
+app.use(express.json());
 
-app.get('/', (req, res) => {
-  res.send('Hello World');
+// Endpoint for add new user
+app.post('/api/v1/users', async (req, res) => {
+  const { name, email, bio } = req.body;
+
+  try {
+    const user = await prisma.user.create({
+      data: {
+        name,
+        email,
+        profile: {
+          create: {
+            bio
+          }
+        }
+      }
+    });
+    res.json(user);
+  } catch (error) {
+    res.status(400).json({ error: 'Error creating user' });
+  }
 });
 
-app.listen(port, () => {
-  console.log(`Server running at http://localhost:${port}`);
+// Start the server
+app.listen(3000, () => {
+  console.log('Server is running on port 3000');
 });
