5c3272e786f1242af43711ed500883e038d33d99
adibnajwan <adibnajwan@gmail.com>
2024-10-29 23:24:51 +0700

    Index.js adjustment to routes directory

--- index.js
+++ index.js
@@ -1,14 +1,14 @@
 const express = require('express');
 const app = express();
-const userController = require('./controllers/userController');
-const accountController = require('./controllers/accountController');
-const transactionController = require('./controllers/transactionController');
+const userRoutes = require('./routes/users');
+const accountRoutes = require('./routes/accounts');
+const transactionRoutes = require('./routes/transactions');
 
 app.use(express.json());
 
-app.use('/api/v1/users', userController);
-app.use('/api/v1/accounts', accountController);
-app.use('/api/v1/transactions', transactionController);
+app.use('/api/v1/users', userRoutes);
+app.use('/api/v1/accounts', accountRoutes);
+app.use('/api/v1/transactions', transactionRoutes);
 
 app.listen(3000, () => {
   console.log('Server is running on port 3000');
