63701f737d316c133f41af3dafd01a691203d2ca
adibnajwan <adibnajwan@gmail.com>
2024-10-29 11:33:45 +0700

    Make uerValidation.js

---
+++ validations/userValidation.js
@@ -0,0 +1,17 @@
+const { body, validationResult } = require('express-validator');
+
+const validateUser = [
+  body('name').isString().withMessage('Name must be a string'),
+  body('email').isEmail().withMessage('Invalid email format'),
+  body('bio').optional().isString(),
+
+  (req, res, next) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
+    }
+    next();
+  },
+];
+
+module.exports = { validateUser };
