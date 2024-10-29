4d1ce9b5b8d0fff984822a57ed2b575fe2e3c8f6
adibnajwan <adibnajwan@gmail.com>
2024-10-29 11:32:06 +0700

    Make userService.js

---
+++ controllers/userService.js
@@ -0,0 +1,29 @@
+const { PrismaClient } = require('@prisma/client');
+const prisma = new PrismaClient();
+
+const createUser = async (data) => {
+  return await prisma.user.create({
+    data: {
+      name: data.name,
+      email: data.email,
+      profile: {
+        create: { bio: data.bio },
+      },
+    },
+  });
+};
+
+const getAllUsers = async () => {
+  return await prisma.user.findMany({
+    include: { profile: true },
+  });
+};
+
+const getUserById = async (userId) => {
+  return await prisma.user.findUnique({
+    where: { id: parseInt(userId) },
+    include: { profile: true },
+  });
+};
+
+module.exports = { createUser, getAllUsers, getUserById };
