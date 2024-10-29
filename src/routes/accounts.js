62a5c88672d1b12fe9141ac49b2f617f178dbc90
adibnajwan <adibnajwan@gmail.com>
2024-10-29 23:22:16 +0700

    Make accounts.js file in routes dir

---
+++ src/routes/accounts.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const router = express.Router();
+const accountController = require('../controllers/accountController');
+
+router.post('/', accountController.createAccount);
+
+router.get('/', accountController.getAllAccounts);
+
+router.get('/:accountId', accountController.getAccountById);
+
+module.exports = router;
