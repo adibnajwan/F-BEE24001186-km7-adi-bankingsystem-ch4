2e43580ceff35130e6baeec9cecd17f38ec3ebea
adibnajwan <adibnajwan@gmail.com>
2024-10-29 23:23:02 +0700

    Make transactions.js  file in routes dir

---
+++ src/routes/transactions.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const router = express.Router();
+const transactionController = require('../controllers/transactionController');
+
+router.post('/', transactionController.createTransaction);
+
+router.get('/', transactionController.getAllTransactions);
+
+router.get('/:transactionId', transactionController.getTransactionById);
+
+module.exports = router;
