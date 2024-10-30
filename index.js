7d0bc9f5d7ae3b1eb4f4d00cecba78f97740089b
adibnajwan <adibnajwan@gmail.com>
2024-10-30 14:34:58 +0700

    Modified index.js, separate the logic for running the server from the logic for exporting the application

--- index.js
+++ index.js
@@ -11,6 +11,11 @@ app.use('/api/v1/users', userRoutes);
 app.use('/api/v1/accounts', accountRoutes);
 app.use('/api/v1/transactions', transactionRoutes);
 
-app.listen(3000, () => {
-  console.log('Server is running on port 3000');
-});
+const PORT = process.env.PORT || 3000;
+if (require.main === module) {
+  app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+  });
+}
+
+module.exports = app;
