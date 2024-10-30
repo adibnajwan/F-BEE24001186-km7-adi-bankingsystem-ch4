1258d4e0ef3299e99b8316af593cf249235e8a60
adibnajwan <adibnajwan@gmail.com>
2024-10-30 16:27:07 +0700

    add test to account.js

--- tests/functional/unit/account.test.js
+++ tests/functional/unit/account.test.js
@@ -1,17 +1,17 @@
 const request = require('supertest');
-const app = require('../index'); 
-const { PrismaClient } = require('@prisma/client');
-const prisma = new PrismaClient();
+const app = require('../../../index'); 
 
-describe('Account API', () => {
-  afterAll(async () => {
-    await prisma.$disconnect();
+describe('User API', () => {
+  it('should retrieve all users', async () => {
+    const res = await request(app).get('/api/v1/users');
+    expect(res.statusCode).toEqual(200);
+    expect(Array.isArray(res.body)).toBe(true);
   });
-  
-  it('should create a new account', async () => {
+
+  it('should create a new user', async () => {
     const res = await request(app)
-      .post('/api/v1/accounts')
-      .send({ userId: 1, accountNumber: '123456789', balance: 1000.0 });
+      .post('/api/v1/users')
+      .send({ name: 'Test User', email: 'testuser@example.com' });
 
     expect(res.statusCode).toEqual(201);
     expect(res.body).toHaveProperty('id');
