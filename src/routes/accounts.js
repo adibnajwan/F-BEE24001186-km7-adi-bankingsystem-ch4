43e25a3e29d0fa058eb1c2a38902f9c3696cc7df
adibnajwan <adibnajwan@gmail.com>
2024-10-31 02:03:44 +0700

    Add middleware to accounts.js

--- src/routes/accounts.js
+++ src/routes/accounts.js
@@ -1,9 +1,11 @@
 const express = require('express');
 const router = express.Router();
 const accountController = require('../controllers/accountController');
+const { authenticateToken } = require('../middleware/authMiddleware');
 
 router.post('/', accountController.createAccount);
 router.get('/', accountController.getAllAccounts);
 router.get('/:accountId', accountController.getAccountById);
+router.get('/', authenticateToken, accountController.getAccountByUser);
 
 module.exports = router;
