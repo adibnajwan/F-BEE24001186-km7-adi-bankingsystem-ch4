b2bc39adfddfa3d512212cc4444f8726b50932bd
adibnajwan <adibnajwan@gmail.com>
2024-10-30 16:05:44 +0700

    add test for creating a new account

--- tests/functional/unit/account.test.js
+++ tests/functional/unit/account.test.js
@@ -7,3 +7,13 @@ describe('Account API', () => {
   afterAll(async () => {
     await prisma.$disconnect();
   });
+  
+  it('should create a new account', async () => {
+    const res = await request(app)
+      .post('/api/v1/accounts')
+      .send({ userId: 1, accountNumber: '123456789', balance: 1000.0 });
+
+    expect(res.statusCode).toEqual(201);
+    expect(res.body).toHaveProperty('id');
+  });
+});
