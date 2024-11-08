0e8ed2398a9cdd440ed9285ff889bb4787969bef
adibnajwan <adibnajwan@gmail.com>
2024-11-08 16:10:24 +0700

    add mock data for user.test.js

--- tests/functional/unit/user.test.js
+++ tests/functional/unit/user.test.js
@@ -1,23 +1,48 @@
 const request = require('supertest');
 const app = require('../../../index'); 
 const { PrismaClient } = require('@prisma/client');
+
+jest.mock('@prisma/client', () => {
+  return {
+    PrismaClient: jest.fn().mockImplementation(() => ({
+      user: {
+        create: jest.fn().mockResolvedValue({
+          id: 1,
+          name: 'Adib',
+          email: 'adib2@gmail.com',
+          bio: 'Communication Visual'
+        }),
+        findMany: jest.fn().mockResolvedValue([
+          { id: 1, name: 'Adib', email: 'adib2@gmail.com', bio: 'Communication Visual' }
+        ]),
+        findUnique: jest.fn().mockResolvedValue({
+          id: 1,
+          name: 'Adib',
+          email: 'adib2@gmail.com',
+          bio: 'Communication Visual'
+        })
+      }
+    }))
+  };
+});
+
 const prisma = new PrismaClient();
 
 describe('User API', () => {
-  afterAll(async () => {
-    await prisma.$disconnect();
+  afterAll(() => {
+    jest.clearAllMocks();
   });
 
   it('should create a new user', async () => {
     const res = await request(app)
       .post('/api/v1/users')
-      .send({ name: 'Adib', email: 'adibnajwan@example.com', bio: 'Backend Engineer' });
-  
-    console.log(res.body); 
+      .send({ name: 'Adib', email: 'adib2@gmail.com', bio: 'Communication Visual' });
+
+    console.log(res.body);
     expect(res.statusCode).toEqual(201);
     expect(res.body).toHaveProperty('id');
     expect(res.body.name).toBe('Adib');
-  });  
+  });
 
   it('should retrieve all users', async () => {
     const res = await request(app).get('/api/v1/users');
@@ -26,7 +51,7 @@ describe('User API', () => {
     expect(res.body).toBeInstanceOf(Array);
   });
 
-    it('should retrieve a user by ID', async () => {
+  it('should retrieve a user by ID', async () => {
     const userId = 1;
     const res = await request(app).get(`/api/v1/users/${userId}`);
 
