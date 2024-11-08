7c6b868c1cb8a1ce98a31673162147e6fe218c04
adibnajwan <adibnajwan@gmail.com>
2024-11-09 02:21:43 +0700

    add some routes for multer in app.js

--- src/app.js
+++ src/app.js
@@ -1,11 +1,10 @@
-// src/app.js
 const express = require("express");
 const morgan = require("morgan");
 const cors = require("cors");
-const app = express();
 const swaggerUi = require("swagger-ui-express");
 const swaggerDocument = require("../docs/swagger.yaml");
 
+const app = express();
 app.use(cors());
 app.use(express.json());
 
@@ -13,11 +12,13 @@ const authRoutes = require("./routes/auths");
 const userRoutes = require("./routes/users");
 const accountRoutes = require("./routes/accounts");
 const transactionRoutes = require("./routes/transactions");
+const mediaRoutes = require("./routes/media");
 
 app.use("/api/v1/auth", authRoutes);
 app.use("/api/v1/users", userRoutes);
 app.use("/api/v1/accounts", accountRoutes);
 app.use("/api/v1/transactions", transactionRoutes);
+app.use('/api/v1/media', mediaRoutes); 
 app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 
 module.exports = app;
