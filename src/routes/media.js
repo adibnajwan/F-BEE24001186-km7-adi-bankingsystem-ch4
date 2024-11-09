ed37ac642692439ecd56fd7a4ba881a940436b30
adibnajwan <adibnajwan@gmail.com>
2024-11-09 23:30:05 +0700

    add some routes for new feature

--- src/routes/media.js
+++ src/routes/media.js
@@ -1,8 +1,17 @@
+// src/routes/media.js
 const express = require('express');
 const router = express.Router();
-const { imageUpload } = require('../libs/multer');  
-const { uploadImage } = require('../controllers/mediaController');
+const { imageUpload } = require('../libs/multer');
+const { uploadImage, getImages, getImageById, deleteImage, updateImage } = require('../controllers/mediaController');
 
 router.post('/images', imageUpload.single('image'), uploadImage);
+
+router.get('/images', getImages);
+
+router.get('/images/:id', getImageById);
+
+router.delete('/images/:id', deleteImage);
+
+router.put('/images/:id', updateImage);
 
 module.exports = router;
