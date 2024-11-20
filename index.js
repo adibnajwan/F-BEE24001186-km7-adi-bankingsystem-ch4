3b2160c0bda635c04e79a8cc7df46dcb12c190a4
adibnajwan <adibnajwan@gmail.com>
2024-11-20 11:06:18 +0700

    add io middleware in index.js

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
 const express = require('express');
 const YAML = require('yamljs');
 const swaggerUi = require('swagger-ui-express');
-const app = express();
 const http = require('http'); 
 const socketIO = require('socket.io');
+const ioMiddleware = require('./src/middleware/ioMiddleware');  
+
+const app = express();
 
 const swaggerDocument = YAML.load('./docs/swagger.yaml'); 
 
@@ -11,17 +13,20 @@ const userRoutes = require('./src/routes/users');
 const accountRoutes = require('./src/routes/accounts');
 const transactionRoutes = require('./src/routes/transactions');
 const mediaRoutes = require('./src/routes/media');  
-const app = require('./src/app'); 
+
 const server = http.createServer(app); 
 const io = socketIO(server);
 
+app.use(ioMiddleware(io));  
+
 app.use(express.json());
+
 app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 
 app.use('/api/v1/users', userRoutes);
 app.use('/api/v1/accounts', accountRoutes);
 app.use('/api/v1/transactions', transactionRoutes);
-app.use('/api/v1/media', mediaRoutes); 
+app.use('/api/v1/media', mediaRoutes);
 
 io.on('connection', (socket) => {
   console.log('New client connected:', socket.id);
@@ -35,7 +40,7 @@ io.on('connection', (socket) => {
 
 const PORT = process.env.PORT || 3000;
 if (require.main === module) {
-  app.listen(PORT, () => {
+  server.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
   });
 }
